0bbda96d50fe7a0f5c3b34da97c622ae00fbf6de
satya <koppulasatyateja@gmail.com>
2026-02-09 16:22:52 +0530

    action

--- 03-variables.sh
+++ 03-variables.sh
@@ -5,5 +5,5 @@ PERSON2=Suresh
 
 echo "Hi PERSON1, How are you?"
 echo "Hi PERSON2, I am good, How are you?"
-echo "I am Good PERSON2, what are you doin
+echo "I am Good PERSON2, what are you doing"
 echo "okay PERSON1, I am learning DevOps."
