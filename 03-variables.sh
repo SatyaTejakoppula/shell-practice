6334902eb9c7a6ff70ee8e553aeb204787f2689e
satya <koppulasatyateja@gmail.com>
2026-02-09 16:20:41 +0530

    action

---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bach
+
+PERSON1=Ramesh
+PERSON2=Suresh
+
+echo "Hi PERSON1, How are you?"
+echo "Hi PERSON2, I am good, How are you?"
+echo "I am Good PERSON2, what are you doin
+echo "okay PERSON1, I am learning DevOps."
