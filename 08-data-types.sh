3f2121628435717b37b4743c033dfc2062bd149e
satya <koppulasatyateja@gmail.com>
2026-02-10 11:40:15 +0530

    action

--- 08-data-types.sh
+++ 08-data-types.sh
@@ -9,3 +9,11 @@ echo "Sum is: $SUM"
 
 #here if we use any text value instead of number in any of the varaible. it treats as 0 and executes the sum 
 
+#Array Example / List
+
+FRUITS=("Apple" "Banana" "Grapes")
+
+echo "FRUITS are: ${FRUITS[@]}"
+echo "First Fruit is: ${FRUITS[0]}" #here index number starts from 0 in a list 
+echo "Second Fruit is: ${FRUITS[1]}"
+echo "Third Fruit is: ${FRUITS[2]}"
