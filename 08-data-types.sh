4426942317756d84a61c0ec161f8bee5b2798e12
satya <koppulasatyateja@gmail.com>
2026-02-10 11:30:37 +0530

    action

---
+++ 08-data-types.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+NUM1=100
+NUM2=200
+
+SUM=$(($NUM1+$NUM2))
+
+echo "Sum is: $SUM"
+
+#here if we use any text value instead of number in any of the varaible. it treats as 0 and executes the sum 
+
