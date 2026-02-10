2163356da94c483219962abdf407cc0e640ffa05
satya <koppulasatyateja@gmail.com>
2026-02-10 10:40:37 +0530

    action

---
+++ 05-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bach
+
+echo "please enter your username::"
+read USER_NAME
+
+echo "username is $USER_NAME"
+
+echo "please enter your password::"
+read -s PASSWORD
+
+echo "password is $PASSWORD"
