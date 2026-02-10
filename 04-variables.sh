0ce8627ad50364af5f407d35dcc749de0867b8cc
satya <koppulasatyateja@gmail.com>
2026-02-10 10:25:19 +0530

    action

--- 04-variables.sh
+++ 04-variables.sh
@@ -4,7 +4,7 @@ PERSON1=$1
 PERSON2=$2
 
 
-echo "Hi PERSON1, How are you?"
-echo "Hi PERSON2, I am good, How are you?"
-echo "I am Good PERSON2, what are you doing"
-echo "okay PERSON1, I am learning DevOps."
+echo "Hi $PERSON1, How are you?"
+echo "Hi $PERSON2, I am good, How are you?"
+echo "I am Good $PERSON2, what are you doing"
+echo "okay $PERSON1, I am learning DevOps."
